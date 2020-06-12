7ca9c862180023e1bddb1cf441679126001bb384
David Cole <davidtcole@yahoo.com>
2020-06-12 14:16:07 -0700

    teach me to read the prompt more closely. There was an inherent flaw in my design and I had to refactor my code to follow the guidelines. One step forward 5 steps back.

--- js/scripts.js
+++ js/scripts.js
@@ -2,37 +2,31 @@ $(document).ready(function() {
   $("#input-form").submit(function(event){
     event.preventDefault(event);
 
-    const userInput = $("input#user-input").val();
+    const userInput = parseInt($("input#user-input").val());
     processInput(userInput);
   });
 });
 
 function processInput(userInput) {
-  const separatedInput = userInput.split("");
-  let convertedArray = separatedInput.map(Number);
+  const numericalArray = [];
+  const one = "1";
+  const two = "2";
+  const three = "3";
 
-  if(convertedArray.includes(NaN)){
+  if (userInput === NaN){
     resetPage();
   } else {
-      if(convertedArray.includes(1)) {
-        for (i = 0; i < convertedArray.length; i++) {
-          if(convertedArray[i] === 1){
-            convertedArray[i] = "Beep!";
+      for(i = 0; i <= userInput; i++) {
+        numericalArray.push(i);
+      }
+      let stringArray = numericalArray.map(String);
+      if (numericalArray.includes("1")) {
+        for (i = 0; i < numericalArray.length; i++) {
+          if (one.indexOf(numericalArray[i]) > -1) {
+            numericalArray[i] = numericalArray.replace(numericalArray[i],"Beep!");
           }
         }
-      } else if(convertedArray.includes(2)) {
-          for (i = 0; i < convertedArray.length; i++) {
-            if(convertedArray[i] === 2){
-              convertedArray[i] = "Boop";
-            }          } 
-        }  else if(convertedArray.includes(3)) {
-            for (i = 0; i < convertedArray.length; i++) {
-              if(convertedArray[i] === 3) {
-                convertedArray[i] = "Won't you be my neighbor?";
-              }
-            } 
-          }
-      console.log(convertedArray);
+      } 
   }
 }
 
