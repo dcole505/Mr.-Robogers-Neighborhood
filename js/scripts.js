aa8b3e40463e00141f976e068fcf3cc7a961dc66
David Cole <davidtcole@yahoo.com>
2020-06-12 12:53:04 -0700

    add new branching conditions to check for user input to include 1, 2, or 3 and reassign the index value to the corresponding output string.

--- js/scripts.js
+++ js/scripts.js
@@ -9,14 +9,29 @@ $(document).ready(function() {
 
 function processInput(userInput) {
   const separatedInput = userInput.split("");
-  const convertedArray = separatedInput.map(Number);
-  console.log(separatedInput);
+  let convertedArray = separatedInput.map(Number);
 
   if(convertedArray.includes(NaN)){
     $("#form-clear").click(function() {
       location.reload(true);
       alert("Please enter valid numerical input!")
     });
-    
+  } else {
+      if(convertedArray.includes(1)) {
+        for (let element of convertedArray) {
+          convertedArray = "Beep!";
+          console.log(convertedArray);
+        }
+      } else if(convertedArray.includes(2)) {
+          for (let element of convertedArray) {
+            convertedArray = "Boop";
+            console.log(convertedArray);
+          } 
+        }  else if(convertedArray.includes(3)) {
+            for (let element of convertedArray) {
+              convertedArray = "Won't you be my neighbor?";
+              console.log(convertedArray);
+            } 
+          }
   }
 }
