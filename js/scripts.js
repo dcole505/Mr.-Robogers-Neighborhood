de7b9779a4bc30fddca5126bbb74484b92d2da00
David Cole <davidtcole@yahoo.com>
2020-06-19 12:40:31 -0700

    fix the error checking for non-numerical user input by changing the button type to number in the index.html file. Also a new button to reset the page, and turned off autocomplete in the form. Lastly, I made my best effort to DRY my code.

--- js/scripts.js
+++ js/scripts.js
@@ -1,49 +1,41 @@
+//User Interface Logic
 $(document).ready(function() {
   $("#input-form").submit(function(event){
     event.preventDefault(event);
     $("#display-result").hide();
 
-    const userInput = parseInt($("input#user-input").val());
+    const userInput = $("input#user-input").val();
     const displayArray = processInput(userInput);
     display(displayArray);
   });
 });
 
+//Business Logic
 function processInput(userInput) {
   const numericalArray = [];
   let stringArray = [];
-  const one = "1";
 
-  if (userInput === NaN){
-    resetPage();
-  } else {
-      for(i = 0; i <= userInput; i++) {
-        numericalArray.push(i);
-      }
-      stringArray = numericalArray.map(String);
-      for (i = 0; i < stringArray.length; i++) {
-        if ((stringArray[i].indexOf("1") > -1) && (!(stringArray[i].includes("2"))) && (!(stringArray[i].includes("3")))) {
-          stringArray[i] = stringArray[i].replace(stringArray[i],"Beep!");
-        } else if ((stringArray[i].indexOf("2") > -1) && (!(stringArray[i].includes("3")))) {
-          stringArray[i] = stringArray[i].replace(stringArray[i],"Boop");
-          } else if (stringArray[i].indexOf("3") > -1) {
-            stringArray[i] = stringArray[i].replace(stringArray[i],"Won't you be my neighbor?");
-            }
-      }
-      return stringArray;
+  for(i = 0; i <= userInput; i++) {
+    numericalArray.push(i);
   }
-}
-
-function resetPage() {
-  $("input#user-input").val("");
-  alert("Please enter valid numerical input!");
-  $("#form-clear").click(function(event) {
-    event.preventDefault();
-    location.reload(true);
-  });
+  stringArray = numericalArray.map(String);
+    for (i = 0; i < stringArray.length; i++) {
+      if ((stringArray[i].indexOf("1") > -1) && (!(stringArray[i].includes("2"))) && (!(stringArray[i].includes("3")))) {
+        stringArray[i] = stringArray[i].replace(stringArray[i],"Beep!");
+      } else if ((stringArray[i].indexOf("2") > -1) && (!(stringArray[i].includes("3")))) {
+        stringArray[i] = stringArray[i].replace(stringArray[i],"Boop");
+        } else if (stringArray[i].indexOf("3") > -1) {
+          stringArray[i] = stringArray[i].replace(stringArray[i],"Won't you be my neighbor?");
+          }
+    }
+  return stringArray;
 }
 
 function display(displayArray) {
   $(".rogers-output").text(displayArray);
   $("#display-result").show();
+  $("#reset-button").click(function() {
+    $("#display-result").hide();
+  });
 }
+
